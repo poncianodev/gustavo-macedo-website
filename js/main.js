313775609c43717bb86dc74ff4c5fdad7910cbef
poncianodev <lucasbponciano@hotmail.com>
2024-01-10 00:03:11 -0300

    create the media session

--- js/main.js
+++ js/main.js
@@ -49,3 +49,39 @@ function rotateOnClick(element) {
     element.classList.remove('rotate');
   }, 1000);
 }
+
+// MEDIA SLIDER
+
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 4,
+    },
+  },
+});
